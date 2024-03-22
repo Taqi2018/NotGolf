830eab6f052ca27759cf556ff79b9a91fe7bdf23
AbdullahKhan1357 <taqi92547@gmail.com>
2024-03-23 03:12:14 +0500

    Script Set

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,41 +4,31 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    private Rigidbody rb;
-    public bool isHit = false;
-    public StickController stickController;
+     private Rigidbody rb;
+     public bool isHit = false;
+     public StickController stickController;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = this.GetComponent<Rigidbody>();
-    }
+     // Start is called before the first frame update
+     void Start()
+     {
+          rb = this.GetComponent<Rigidbody>();
+     }
 
-    // Update is called once per frame
-    void Update()
-    {
+     private void OnCollisionEnter(Collision collision)
+     {
+          if (collision.gameObject.CompareTag("stick") && !isHit)
+          {
+               isHit = true;
+               Debug.Log("hit");
+               LaunchBall();
+          }
+     }
 
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("stick") && !isHit)
-        {
-            isHit = true;
-            Debug.Log("hit");
-            LaunchBall();
-        }
-
-
-    }
-
-
-
-    void LaunchBall()
-    {
-        float force = stickController.SwipeForce;
-        Debug.Log(force);
-        rb.AddForce(new Vector3(0, force, force), ForceMode.Impulse);
-    }
+     void LaunchBall()
+     {
+          float force = stickController.SwipeForce;
+          Debug.Log(force);
+          rb.AddForce(new Vector3(0, force, force), ForceMode.Impulse);
+     }
 }
--- Assets/Scripts/StickController.cs
+++ Assets/Scripts/StickController.cs
@@ -8,127 +8,88 @@ using UnityEngine.InputSystem.Controls;
 
 public class StickController : MonoBehaviour
 {
-    InputHub swipeInput;
-    public BallController ballController;
-    public float startXAngle;
-  
-    Vector2 initialPos;
-    private bool isTouch;
-    public float swipePowerMultiplier = 2f;
-    float currentRotation;
-    private bool getTapPos;
-    public float rotationSpeed;
+     InputHub swipeInput;
+     public BallController ballController;
+     public float startXAngle;
 
-    public float SwipeForce;
-
-
+     Vector2 initialPos;
+     private bool isTouch;
+     public float swipePowerMultiplier = 2f;
+     float currentRotation;
+     private bool getTapPos;
+     public float rotationSpeed;
+     public float SwipeForce;
+     public Touch Touch { get; private set; }
 
 
-    public Touch Touch { get; private set; }
+     // Start is called before the first frame update
+     void Start()
+     {
+          swipeInput = new InputHub();
+          swipeInput.Enable();
+          swipeInput.BallSwipe.Tap.started += OnSwipeStart;
+          swipeInput.BallSwipe.Tap.canceled += OnSwipeEnd;
+          swipeInput.BallSwipe.TapPosition.started += OnTapPos;
+          getTapPos = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-  
-        swipeInput = new InputHub();
-        swipeInput.Enable();
-        swipeInput.BallSwipe.Tap.started += OnSwipeStart;
-        swipeInput.BallSwipe.Tap.canceled += OnSwipeEnd;
-        swipeInput.BallSwipe.TapPosition.started += OnTapPos;
-        getTapPos = false;
+     }
 
-    }
+     private void OnTapPos(InputAction.CallbackContext obj)
+     {
+          getTapPos = true;
 
-    private void OnTapPos(InputAction.CallbackContext obj)
-    {
-        getTapPos = true;
+          initialPos = swipeInput.BallSwipe.TapPosition.ReadValue<Vector2>();
 
-        initialPos = swipeInput.BallSwipe.TapPosition.ReadValue<Vector2>();
+          isTouch = true;
+     }
 
-        isTouch = true;
-    }
+     private void OnSwipeEnd(InputAction.CallbackContext obj)
+     {
+          isTouch = false;
+     }
 
-    private void OnSwipeEnd(InputAction.CallbackContext obj)
-    {
+     private void OnSwipeStart(InputAction.CallbackContext obj)
+     {
+          this.transform.rotation = Quaternion.Euler(startXAngle, 0, 0);
+          if (getTapPos)
+          {
+               initialPos = swipeInput.BallSwipe.TapPosition.ReadValue<Vector2>();
 
+               getTapPos = false;
+          }
 
-        isTouch = false;
+     }
 
+     // Update is called once per frame
+     void Update()
+     {
+          if (isTouch)
+          {
+               Vector2 currentPos = swipeInput.BallSwipe.TapPosition.ReadValue<Vector2>();
 
-    }
+               Vector2 swipeDelta = currentPos - initialPos;
 
-    private void OnSwipeStart(InputAction.CallbackContext obj)
-    {
+               if (swipeDelta.y != 0)
+               {
+                    currentRotation = transform.rotation.eulerAngles.x - swipeDelta.y * Time.deltaTime * rotationSpeed;
 
+                    SwipeForce = swipeDelta.y * Time.deltaTime * rotationSpeed;
 
-        this.transform.rotation = Quaternion.Euler(startXAngle, 0, 0);
+                    float clampedRotation;
 
-        if (getTapPos)
-        {
+                    if (currentRotation > 180f)
+                         currentRotation -= 360f;
+                    else if (currentRotation < -180f)
+                         currentRotation += 360f;   //  -180  -60    90   180 
 
-            initialPos = swipeInput.BallSwipe.TapPosition.ReadValue<Vector2>();
+                    // Clamp rotation between -60 and 90 degrees
+                    clampedRotation = Mathf.Clamp(currentRotation, -60f, 90f);
 
-            getTapPos = false;
-        }
-
-    }
-
-    private void FixedUpdate()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-
-
-
-
-        if (isTouch)
-        {
-            Vector2 currentPos = swipeInput.BallSwipe.TapPosition.ReadValue<Vector2>();
-
-            Vector2 swipeDelta = currentPos - initialPos;
-
-
-
-            if (swipeDelta.y != 0)
-            {
-
-
-                currentRotation = transform.rotation.eulerAngles.x - swipeDelta.y * Time.deltaTime * rotationSpeed;
-
-              
-
-                SwipeForce = swipeDelta.y * Time.deltaTime * rotationSpeed;
-
-
-
-
-                float clampedRotation;
-      
-
-                if (currentRotation > 180f)
-                    currentRotation -= 360f;
-                else if (currentRotation < -180f)
-                    currentRotation += 360f;   //  -180  -60    90   180 
-
-
-                // Clamp rotation between -60 and 90 degrees
-                clampedRotation = Mathf.Clamp(currentRotation, -60f, 90f);
-
-                transform.rotation = Quaternion.Euler(clampedRotation, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-
-
-
-            }
-
-            initialPos = currentPos;
-        }
-    }
+                    transform.rotation = Quaternion.Euler(clampedRotation, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+               }
+               initialPos = currentPos;
+          }
+     }
 }
 
 
